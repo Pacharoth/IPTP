03830834098bd08198db48c5990d65b19a263949
roeun <pacharothroeun01@gmail.com>
2021-05-09 07:38:10 +0700

    [Task] Change v-if to v-else-if

--- src/components/Shape.vue
+++ src/components/Shape.vue
@@ -1,9 +1,9 @@
 <template>
     <heart v-if="name=='Heart'" :color="color"></heart>
-    <diamond v-if="name=='Diamond'" :color="color"></diamond>
-    <star v-if="name=='Star'" :color="color"></star>
-    <traingle v-if="name=='Traingle'" :color="color"></traingle>
-    <hexagone v-if="name=='Hexagone'" :color="color"></hexagone>
+    <diamond v-else-if="name=='Diamond'" :color="color"></diamond>
+    <star v-else-if="name=='Star'" :color="color"></star>
+    <traingle v-else-if="name=='Traingle'" :color="color"></traingle>
+    <hexagone v-else-if="name=='Hexagone'" :color="color"></hexagone>
     
 </template>
 <script>
